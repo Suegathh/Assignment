949ebc5e6a742fc476d5f3116dc819e43f78c85e
Suegathh <suzzainnegathu@gmail.com>
2024-10-28 01:46:00 -0700

    javascript

---
+++ assignment.js
@@ -0,0 +1,29 @@
+let year = 2021
+if (year % 4==0){
+    console.log('leap year')
+} else{
+    console.log('not a leap year')
+}
+let price = 10
+if(price >= 100){
+    console.log('discount is 20')
+
+} else if(price >= 50){
+    console.log('discount is 10')
+}
+else{
+    console.log('discount is 0')
+}
+let age = 20
+if(age < 12){
+    console.log('ticket price is 5')
+}
+else if(age < 18){
+    console.log('ticket price is 10')
+}
+else if(age < 60){
+    console.log('ticket price is 20')
+}
+else{
+    console.log('ticket price is 15')
+}
